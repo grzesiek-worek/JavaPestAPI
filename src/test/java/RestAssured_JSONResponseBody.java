cc977f26fa134e30f5b2b422ed2a58e0d2b2e699
grzesiek-worek <grzesiekw@interia.eu>
2023-06-05 11:51:52 +0200

    JSON responseBody - add verify region in JSON response - JSON Path

--- src/test/java/RestAssured_JSONResponseBody.java
+++ src/test/java/RestAssured_JSONResponseBody.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
@@ -20,5 +21,18 @@ public class RestAssured_JSONResponseBody {
 
         String bodyAsString = body.asString();
         Assert.assertEquals(bodyAsString.toLowerCase().contains("acklam") , true , "Response body contains Acklam");
+    }
+
+    @Test
+    public void VerifyRegionInJsonResponse()
+    {
+        RestAssured.baseURI = "https://ipinfo.io/";
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.get("86.10.4.126");
+
+        JsonPath jsonPathEvaluator = response.jsonPath();
+        String region = jsonPathEvaluator.get("region");
+        System.out.println("Region received from Response " + region);
+        Assert.assertEquals(region, "England", "Correct region name received in the Response");
     }
 }
