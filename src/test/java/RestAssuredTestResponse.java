3623ff0e6eab1bd73017330643befbc544b43542
grzesiek-worek <grzesiekw@interia.eu>
2023-06-05 11:01:01 +0200

    add RestAssuredTestResponse task

--- src/test/java/RestAssuredTestResponse.java
+++ src/test/java/RestAssuredTestResponse.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class RestAssuredTestResponse {
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+
+
+public class RestAssuredTestResponse {
+    @Test
+    public void GetBookDetails()
+    {
+        RestAssured.baseURI = "https://demoqa.com/BookStore/v1/Books";
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.get("");
+        int statusCode = response.getStatusCode();
+
+        System.out.println(statusCode);
+        Assert.assertEquals(statusCode , 200 , "Correct status code returned");
+
+    }
 }
